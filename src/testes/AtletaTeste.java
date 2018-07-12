fb79b545e7f638a0147938cd3dfd29e15c6a1abb
Andrei <andreipaesfigueiredo@gmail.com>
2018-07-12 11:38:24 -0300

    AtletaTesteFalha/Sucesso

--- src/testes/AtletaTeste.java
+++ src/testes/AtletaTeste.java
@@ -3,6 +3,7 @@ package testes;
 import dominio.AtletaMT;
 import exceptions.ExceptionDadosIncompletos;
 import exceptions.MatriculaInvalidaException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.ResultSet;
@@ -12,41 +13,73 @@ import static dominio.AtletaMT.cadastrarAtleta;
 import static dominio.AtletaMT.listarAtletas;
 import static dominio.AtletaMT.transferirAtleta;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AtletaTeste {
-    //EXPECTED DADOSINCOMPLETOS
     @Test
-    public void testeCadastrarAtletasNull() throws ClassNotFoundException, SQLException, MatriculaInvalidaException {
+    public void testeCadastrarAtletaNull() throws ClassNotFoundException, SQLException, MatriculaInvalidaException {
         boolean sucesso = true;
         try {
             AtletaMT.cadastrarAtleta("teste", "teste", "teste", "", "", "", "teste");
         }catch (ExceptionDadosIncompletos exceptionDadosIncompletos){
             sucesso = false;
         }
+        assertFalse(sucesso);
     }
-    //EXPECTED DADOSINCOMPLETOS
     @Test
-    public void testeTransferirAtleta() throws ClassNotFoundException, SQLException {
+    public void testeCadastrarAtleta() throws SQLException, MatriculaInvalidaException, ClassNotFoundException {
+        boolean sucesso = false;
+        try {
+            AtletaMT.cadastrarAtleta("testeAtleta", "testeAtleta", "testeAtleta", "testeAtleta", "testeAtleta", "testeAtleta", "362019432");
+        }catch (ExceptionDadosIncompletos exceptionDadosIncompletos){
+            sucesso = false;
+        }
+        ResultSet res = AtletaMT.listarAtletas();
+        while(res.next()){
+            if(res.getString("nome").equals("testeAtleta")) {
+                sucesso = true;
+                break;
+            }
+        }
+        assertTrue(sucesso);
+    }
+    @Test
+    public void testeTransferirAtletaNull() throws ClassNotFoundException, SQLException, MatriculaInvalidaException {
         boolean sucesso = true;
         try {
-            AtletaMT.transferirAtleta("teste", "teste", "teste","","teste","teste");
+            AtletaMT.transferirAtleta("teste", "teste", "teste","","320133379","362019432");
+        }catch(ExceptionDadosIncompletos exceptionDadosIncompletos) {
+            sucesso = false;
+        }
+        assertFalse(sucesso);
+    }
+    @Test
+    public void testeTransferirAtletaMatNull() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos {
+        boolean sucesso = true;
+        try {
+            AtletaMT.transferirAtleta("teste", "teste", "teste","teste","320133379","");
+        }catch(MatriculaInvalidaException e) {
+            sucesso = false;
+        }
+        assertFalse(sucesso);
+    }
+    @Test
+    public void testeTransferirAtleta() throws SQLException, ClassNotFoundException {
+        boolean sucesso = false;
+        try {
+            AtletaMT.transferirAtleta("testeAtletaTransferir", "testeAtletaTransferir", "testeAtletaTransferir","testeAtletaTransferir","320133379","362019432");
         }catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
             sucesso = false;
-        }catch (MatriculaInvalidaException MatriculaInvalidaException) {
+        }catch(MatriculaInvalidaException e){
             sucesso = false;
         }
-
-        assertFalse(sucesso);
-    }
-    //EXPECTED DADOSINCOMPLETOS
-    @Test
-    public void testeTransferirAtletaMat() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos {
-        boolean sucesso = true;
-        try {
-            AtletaMT.transferirAtleta("teste", "teste", "teste","teste","","teste");
-        } catch (MatriculaInvalidaException e) {
-            sucesso = false;
+        ResultSet res = AtletaMT.getDadosAtleta("320133379");
+        while(res.next()){
+            if(res.getString("numero").equals("testeAtletaTransferir")) {
+                sucesso = true;
+                break;
+            }
         }
-        assertFalse(sucesso);
+        assertTrue(sucesso);
     }
 }
