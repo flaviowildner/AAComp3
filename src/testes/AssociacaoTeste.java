f3de26bab7dbaa71624ee34fc097b45397fdc05e
Andrei <andreipaesfigueiredo@gmail.com>
2018-07-11 21:02:44 -0300

    AtletaTeste

--- src/testes/AssociacaoTeste.java
+++ src/testes/AssociacaoTeste.java
@@ -11,10 +11,16 @@ import static dominio.AssociacaoMT.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssociacaoTeste {
+    //EXPECTED DADOSINCOMPLETOS
     @Test
     public void testeCadastrarAssociacaoNull() throws ExceptionDadosIncompletos, SQLException, ClassNotFoundException {
         boolean sucesso = true;
         cadastrarAssociacao("teste", "teste", "teste", "", "", "", "teste");
+        ResultSet res = listarAssociacao();
+        while(res.next()){
+            if(res.getString("nome") == "teste") sucesso = false;
+            break;
+        }
         assertFalse(sucesso);
     }
     //EXCEPTION CLASSNOTFOUND
@@ -22,18 +28,30 @@ public class AssociacaoTeste {
     public void testeCadastrarAssociacao() throws ExceptionDadosIncompletos, SQLException, ClassNotFoundException {
         boolean sucesso = false;
         cadastrarAssociacao("teste", "teste", "teste", "teste", "teste", "teste", "teste");
+        ResultSet res = listarAssociacao();
+        while(res.next()){
+            if(res.getString("nome") == "teste") sucesso = true;
+            break;
+        }
         assertTrue(sucesso);
     }
+
     @Test
     public void testeListarAsssociacaoOrdenado() throws SQLException {
         ResultSet sucesso = AssociacaoMT.listarAssociacao();
         assertNull(sucesso);
 
     }
+    //EXPECTED DADOSINCOMPLETOS
     @Test
     public void testeAlterAssociacaoDados() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos {
         boolean sucesso = true;
         alterarAssociacaoDados("teste","teste", "teste", "", "", "", "teste","362019432");
+        ResultSet res = listarAssociacao();
+        while(res.next()){
+            if(res.getString("nome") == "teste") sucesso = false;
+            break;
+        }
         assertFalse(sucesso);
     }
 }
--- src/testes/AtletaTeste.java
+++ src/testes/AtletaTeste.java
@@ -1,5 +1,53 @@
 package testes;
 
-public class AtletaTeste {
+import exceptions.ExceptionDadosIncompletos;
+import exceptions.MatriculaInvalidaException;
+import org.junit.jupiter.api.Test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static dominio.AtletaMT.cadastrarAtleta;
+import static dominio.AtletaMT.listarAtletas;
+import static dominio.AtletaMT.transferirAtleta;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AtletaTeste {
+    //EXPECTED DADOSINCOMPLETOS
+    @Test
+    public void testeCadastrarAtletasNull() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos, MatriculaInvalidaException {
+        boolean sucesso = true;
+        cadastrarAtleta("teste", "teste", "teste", "", "", "", "teste");
+        ResultSet res = listarAtletas();
+        while(res.next()){
+            if(res.getString("nome") == "teste") sucesso = false;
+            break;
+        }
+        assertFalse(sucesso);
+    }
+    //EXPECTED DADOSINCOMPLETOS
+    @Test
+    public void testeTransferirAtleta() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos, MatriculaInvalidaException {
+        boolean sucesso = true;
+        transferirAtleta("teste", "teste", "teste","","teste","teste");
+        ResultSet res = listarAtletas();
+        while(res.next()){
+            if(res.getString("matricula_associacao") == "teste") sucesso = false;
+            break;
+        }
+        assertFalse(sucesso);
+    }
+    //EXPECTED DADOSINCOMPLETOS
+    @Test
+    public void testeTransferirAtletaMat() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos, MatriculaInvalidaException {
+        boolean sucesso = true;
+        transferirAtleta("teste", "teste", "teste","teste","","teste");
+        ResultSet res = listarAtletas();
+        while(res.next()){
+            if(res.getString("matricula_associacao") == "teste") sucesso = false;
+            break;
+        }
+        assertFalse(sucesso);
+    }
 }
