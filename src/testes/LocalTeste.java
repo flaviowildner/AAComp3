b1b41e4fee310a9ed7a0268c9c67a5d2ca3f8a6a
Andrei <andreipaesfigueiredo@gmail.com>
2018-07-12 12:39:24 -0300

    LocalTesteFalha/Sucesso

--- src/testes/LocalTeste.java
+++ src/testes/LocalTeste.java
@@ -4,13 +4,15 @@ import dominio.LocalMT;
 import exceptions.ExceptionDadosIncompletos;
 import org.junit.jupiter.api.Test;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LocalTeste {
     @Test
-    public void testeCadastrarLocal() throws SQLException, ClassNotFoundException {
+    public void testeCadastrarLocalNull() throws SQLException, ClassNotFoundException {
         boolean sucesso = true;
         try {
             LocalMT.cadastrarLocal("teste2", "", "");
@@ -18,5 +20,22 @@ public class LocalTeste {
             sucesso = false;
         }
         assertFalse(sucesso);
+    }
+    @Test
+    public void testeCadastrarLocal() throws SQLException, ClassNotFoundException {
+        boolean sucesso = false;
+        try {
+            LocalMT.cadastrarLocal("testeLocal", "testeLocal", "50");
+        }catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
+            sucesso = false;
+        }
+        ResultSet res = LocalMT.listarLocais();
+        while(res.next()){
+            if(res.getString("nomeLocal").equals("testeLocal")) {
+                sucesso = true;
+                break;
+            }
+        }
+        assertTrue(sucesso);
     }
 }
