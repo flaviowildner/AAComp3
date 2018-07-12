de909882a88d63fb330ab46fa9c4c79d659ec31d
flaviowildner <flaviowildner@hotmail.com>
2018-07-12 20:34:16 -0300

    Correção AssociacaoTeste

--- src/testes/AssociacaoTeste.java
+++ src/testes/AssociacaoTeste.java
@@ -3,61 +3,88 @@ package testes;
 import dominio.AssociacaoMT;
 import exceptions.ExceptionDadosIncompletos;
 import org.junit.jupiter.api.Test;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssociacaoTeste {
-    //EXPECTED DADOSINCOMPLETOS
     @Test
-    public void testeCadastrarAssociacaoNull() throws SQLException, ClassNotFoundException {
-        boolean sucesso = false;
+    public void testeCadastrarAssociacaoNull() throws  SQLException, ClassNotFoundException {
+        boolean sucesso = true;
         try {
             AssociacaoMT.cadastrarAssociacao("teste", "teste", "teste", "", "", "", "teste");
-        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
             sucesso = true;
-        }
-        ResultSet res = AssociacaoMT.listarAssociacao();
-        while(res.next()){
-            if(res.getString("nome") == "teste") sucesso = false;
-            break;
+        }catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
+            sucesso = false;
         }
         assertFalse(sucesso);
     }
-    //EXCEPTION CLASSNOTFOUND
     @Test
-    public void testeCadastrarAssociacao() throws ExceptionDadosIncompletos, SQLException, ClassNotFoundException {
+    public void testeCadastrarAssociacao() throws SQLException, ClassNotFoundException {
         boolean sucesso = false;
-        AssociacaoMT.cadastrarAssociacao("teste", "teste", "teste", "teste", "teste", "teste", "teste");
+        try {
+            AssociacaoMT.cadastrarAssociacao("testeAssociacao", "testeAssociacao", "testeAssociacao", "testeAssociacao", "testeAssociacao", "testeAssociacao", "testeAssociacao");
+        }catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
+            sucesso = false;
+        }
         ResultSet res = AssociacaoMT.listarAssociacao();
         while(res.next()){
-            System.out.println(res.getString("nome"));
-            if(res.getString("nome").equals("teste")){
+            if(res.getString("nome").equals("testeAssociacao")) {
                 sucesso = true;
                 break;
             }
         }
         assertTrue(sucesso);
     }
-
     @Test
     public void testeListarAsssociacaoOrdenado() throws SQLException {
-        ResultSet sucesso = AssociacaoMT.listarAssociacao();
-        assertNull(sucesso);
-
-    }
-    //EXPECTED DADOSINCOMPLETOS
-    @Test
-    public void testeAlterAssociacaoDados() throws ClassNotFoundException, SQLException, ExceptionDadosIncompletos {
-        boolean sucesso = true;
-        AssociacaoMT.alterarAssociacaoDados("teste","teste", "teste", "", "", "", "teste","362019432");
+        boolean sucesso = false;
         ResultSet res = AssociacaoMT.listarAssociacao();
+        ArrayList<String> nomes = new ArrayList<String>();
+        while(res.next()) {
+            nomes.add(res.getString("nome"));
+        }
+        Collections.sort(nomes);
+        int i = 0;
+        res.beforeFirst();
         while(res.next()){
-            if(res.getString("nome") == "teste") sucesso = false;
-            break;
+            if(!res.getString("nome").equals(nomes.get(i))) {
+                sucesso = false;
+                break;
+            }
+            i++;
+            sucesso = true;
+        }
+        assertTrue(sucesso);
+    }
+    @Test
+    public void testeAlterarAssociacaoDadosNull() throws ClassNotFoundException, SQLException{
+        boolean sucesso = true;
+        try {
+            AssociacaoMT.alterarAssociacaoDados("teste","teste", "teste", "", "", "", "teste","362019432");
+        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
+            sucesso = false;
         }
         assertFalse(sucesso);
+    }
+    @Test
+    public void testeAlterarAssociacaoDados() throws ClassNotFoundException, SQLException{
+        boolean sucesso = false;
+        try {
+            AssociacaoMT.alterarAssociacaoDados("testeAssociacaoAlterar","testeAssociacaoAlterar", "testeAssociacaoAlterar", "testeAssociacaoAlterar", "testeAssociacaoAlterar", "testeAssociacaoAlterar", "testeAssociacaoAlterar","362019432");
+        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
+            sucesso = false;
+        }
+        ResultSet res = AssociacaoMT.listarAssociacao();
+        while(res.next()){
+            if(res.getString("nome").equals("testeAssociacaoAlterar")) {
+                sucesso = true;
+                break;
+            }
+        }
+        assertTrue(sucesso);
     }
 }
