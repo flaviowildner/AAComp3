dff67d960289cda886810dd939074a1ec9760aef
Andrei <andreipaesfigueiredo@gmail.com>
2018-07-14 21:09:57 -0300

    Prova Testes

--- src/testes/testesUnitarios/ProvaTesteUnitario.java
+++ src/testes/testesUnitarios/ProvaTesteUnitario.java
@@ -23,29 +23,22 @@ public class ProvaTesteUnitario {
         }
         assertFalse(sucesso);
     }
-//    @Test
-//    public void testeCadastrarProva() throws SQLException {
-//        boolean sucesso = false;
-//        try {
-//            ProvaMT.cadastrarProva("testeProva", "testeProva", "testeProva", "testeCompeticaoAlterar");
-//        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
-//            sucesso = false;
-//        }
-//        ResultSet res = ProvaMT.getProvasCompeticao("testeCompeticaoAlterar");
-//        while(res.next()){
-//            if(res.getString("nome").equals("testeProva")) {
-//                sucesso = true;
-//                break;
-//            }
-//        }
-//        assertTrue(sucesso);
-//    }
     @Test
-    public void testeInscreverAtletaProvaMat() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, ExceptionDadosIncompletos {
+    public void testeInscreverAtletaProvaMatNull() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, ExceptionDadosIncompletos {
         boolean sucesso = true;
         try {
             ProvaMT.inscreverAtletaProva("", "teste");
-        }catch (MatriculaInvalidaException e){
+        } catch (MatriculaInvalidaException e) {
+            sucesso = false;
+        }
+        assertFalse(sucesso);
+    }
+    @Test
+    public void testeInscreverAtletaProvaMatInvalida() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, ExceptionDadosIncompletos {
+        boolean sucesso = true;
+        try {
+            ProvaMT.inscreverAtletaProva("matriculaInvalida", "teste");
+        } catch (MatriculaInvalidaException e) {
             sucesso = false;
         }
         assertFalse(sucesso);
@@ -54,27 +47,10 @@ public class ProvaTesteUnitario {
     public void testeInscreverAtletaProvaNull() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, MatriculaInvalidaException {
         boolean sucesso = true;
         try {
-            ProvaMT.inscreverAtletaProva("682705664","");
+            ProvaMT.inscreverAtletaProva("682705664", "");
         } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
             sucesso = false;
         }
         assertFalse(sucesso);
     }
-//    @Test
-//    public void testeInscreverAtletaProva() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, MatriculaInvalidaException {
-//        boolean sucesso = false;
-//        try {
-//            ProvaMT.inscreverAtletaProva("682705664","testeProva");
-//        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
-//            sucesso = false;
-//        }
-//        ResultSet res = ProvaMT.getProvasCompeticao("testeCompeticaoAlterar");
-//        while(res.next()){
-//            if(res.getString("nome").equals("testeProva")) {
-//                sucesso = true;
-//                break;
-//            }
-//        }
-//        assertTrue(sucesso);
-//    }
 }
