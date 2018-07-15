ac3c9c6025d44251be69c1ecb431b2c02a893b15
Andrei <andreipaesfigueiredo@gmail.com>
2018-07-14 21:27:51 -0300

    Correção testeUnitarioProva

--- src/testes/testesUnitarios/ProvaTesteUnitario.java
+++ src/testes/testesUnitarios/ProvaTesteUnitario.java
@@ -1,56 +1,38 @@
 package testes.testesUnitarios;
 
-import dominio.ProvaMT;
 import exceptions.AtletaJaInscritoEmProvaException;
 import exceptions.ExceptionDadosIncompletos;
 import exceptions.JaExisteException;
 import exceptions.MatriculaInvalidaException;
 import org.junit.jupiter.api.Test;
+import testes.Mocks.MockProva;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProvaTesteUnitario {
     @Test
     public void testeCadastrarProvaNull() throws ClassNotFoundException, SQLException, JaExisteException {
-        boolean sucesso = true;
-        try {
-            ProvaMT.cadastrarProva("teste", "", "", "");
-        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
-            sucesso = false;
-        }
-        assertFalse(sucesso);
+        MockProva testeProva = new MockProva();
+        testeProva.cadastrarProva("testeProva","testeProva","", "testeProva");
+        testeProva.validateFail();
     }
     @Test
     public void testeInscreverAtletaProvaMatNull() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, ExceptionDadosIncompletos {
-        boolean sucesso = true;
-        try {
-            ProvaMT.inscreverAtletaProva("", "teste");
-        } catch (MatriculaInvalidaException e) {
-            sucesso = false;
-        }
-        assertFalse(sucesso);
+        MockProva testeProva = new MockProva();
+        testeProva.inscreverAtletaProva("","testeProva");
+        testeProva.validateFail();
     }
     @Test
     public void testeInscreverAtletaProvaMatInvalida() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, ExceptionDadosIncompletos {
-        boolean sucesso = true;
-        try {
-            ProvaMT.inscreverAtletaProva("matriculaInvalida", "teste");
-        } catch (MatriculaInvalidaException e) {
-            sucesso = false;
-        }
-        assertFalse(sucesso);
+        MockProva testeProva = new MockProva();
+        testeProva.inscreverAtletaProva("matriculaInvalida","testeProva");
+        testeProva.validateFail();
     }
     @Test
     public void testeInscreverAtletaProvaNull() throws ClassNotFoundException, SQLException, AtletaJaInscritoEmProvaException, MatriculaInvalidaException {
-        boolean sucesso = true;
-        try {
-            ProvaMT.inscreverAtletaProva("682705664", "");
-        } catch (ExceptionDadosIncompletos exceptionDadosIncompletos) {
-            sucesso = false;
-        }
-        assertFalse(sucesso);
+        MockProva testeProva = new MockProva();
+        testeProva.inscreverAtletaProva("testeProva","");
+        testeProva.validateFail();
     }
 }
